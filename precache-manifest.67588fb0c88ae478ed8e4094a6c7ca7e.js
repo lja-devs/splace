09713e76281ff49879a60f1aaac42b12753b934b
Lucas-JAOUEN <lucas.jaouen1@gmail.com>
2021-12-24 19:17:57 +0100

    New Deployment

---
+++ precache-manifest.67588fb0c88ae478ed8e4094a6c7ca7e.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "16d593dc027f7eb811ad",
+    "url": "/splace.vue/css/app.e46c72c2.css"
+  },
+  {
+    "revision": "82b9c7a5a3f405032b1db71a25f67021",
+    "url": "/splace.vue/img/logo.82b9c7a5.png"
+  },
+  {
+    "revision": "5a9a43bf7bd121cb00804f43549650ba",
+    "url": "/splace.vue/index.html"
+  },
+  {
+    "revision": "c5383bfe57216ce30dc2",
+    "url": "/splace.vue/js/about.877fc471.js"
+  },
+  {
+    "revision": "16d593dc027f7eb811ad",
+    "url": "/splace.vue/js/app.1b75dfb6.js"
+  },
+  {
+    "revision": "d77828f45d8f1dcf41ba",
+    "url": "/splace.vue/js/chunk-vendors.f9203cc6.js"
+  },
+  {
+    "revision": "ecdd80d497577b28fbdb9e0593233edf",
+    "url": "/splace.vue/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/splace.vue/robots.txt"
+  }
+]);
